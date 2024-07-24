77b45065afff7656c182c97fbc368eae19ccc1b0
paepilai <ioonooni@gmail.com>
2024-07-24 13:40:46 +0700

    adding user profile feature

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -2,13 +2,25 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  const { isAuthenticated } = useAuth0();
+  // const { isAuthenticated } = useAuth0();
 
-  return isAuthenticated ? (
-    <Outlet></Outlet>
-  ) : (
-    <Navigate to="/" replace></Navigate>
-  );
+  // return isAuthenticated ? (
+  //   <Outlet></Outlet>
+  // ) : (
+  //   <Navigate to="/" replace></Navigate>
+  // );
+
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return null;
+  }
+
+  if (isAuthenticated) {
+    return <Outlet />;
+  }
+
+  return <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute;
