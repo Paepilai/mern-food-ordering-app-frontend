c44d614ba580663225d494ba8187d1d16fc668ee
paepilai <ioonooni@gmail.com>
2024-07-26 14:21:27 +0700

    added checkout flow

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,7 +1,8 @@
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { MenuItem as MenuItemType } from "@/types";
 
 type Props = {
-  menuItem: MenuItem;
+  menuItem: MenuItemType;
   addToCart: () => void;
 };
 
